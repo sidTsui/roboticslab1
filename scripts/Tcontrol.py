726979972557e19dc619d34e00a9fe454facd237
sidTsui <147187017+sidTsui@users.noreply.github.com>
2024-02-06 11:26:08 -0500

    Update Tcontrol.py

--- scripts/Tcontrol.py
+++ scripts/Tcontrol.py
@@ -8,21 +8,20 @@ from turtlesim.msg import Pose
 from robotics_lab_1.msg import Turtlecontrol
 
 #initializing pose and turtlecontrol
-pos_msg = Pose() 
+pose_msg = Pose() 
 control_msg = Turtlecontrol();
 
 
 
-def pos_callback(data):#position info
+def pos_callback(data):#position info, used pos_publisher.py from example code
 	global pos_msg
 	#convert linear position to degrees and store in pose obj
-	pos_msg.x = data.x 
+	pos_msg.xd = data.xd 
 	
-def control_gain(data):#control gain info
+def control_gain(data):#control gain info., used pos_callback from example code as an outline
 	global control_msg 
 	#get control gain values and store them in turtlecontrol
 	control_msg.kp = data.kp
-	control_msg.xd = data.xd
 	
 
 if __name__ == '__main__':
@@ -31,9 +30,9 @@ if __name__ == '__main__':
 	# initialize the node
 	rospy.init_node('ControlNode', anonymous = True)
 	# set a 10Hz frequency for this loop
-	rospy.Subscriber('/turtle1/pose',Pose, pose_callback)
+	rospy.Subscriber('/turtle1/pose',pos_msg, pose_callback)
 	#subcriber to turtle1/pose topic, taking in pose
-	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, control_gain)
+	rospy.Subscriber('/turtle1/control_params', control_msg, control_gain)
 	#subcriber to turtle1/control_params topic, taking in turtlecontrol
 	loop_rate = rospy.Rate(10)
 	# declare a variable of type Twist for sending control commands
@@ -41,7 +40,7 @@ if __name__ == '__main__':
 	# run this control loop regularly
 	while not rospy.is_shutdown():
 		# set the linear (forward/backward) velocity command
-		vel_cmd.linear.x = control_msg.kp * (control_msg.xd - control_msg.x)
+		vel_cmd.linear.x = control_msg.kp * (control_msg.xd - control_msg.x) #formula from assignment page
 		# publish the command to the defined topic
 		cmd_pub.publish(vel_cmd)
 		# wait for 0.1 seconds until the next loop and repeat
